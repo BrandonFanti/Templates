86268bb58dc162cf66168a6eacac90335a60e282
Brandon Fanti <BrandonFanti@protonmail.com>
2025-06-23 10:31:27 -0400

    update django_mamba template

--- Python/mamba/template/templates/project_type/django/src/django_mamba/django_mamba_app/urls.py
+++ Python/mamba/template/templates/project_type/django/src/django_mamba/django_mamba_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views, app_name
 
 urlpatterns = [
-    path("", views.index, name=app_name),
-    path("login", views.login, name=app_name),
+    path("",  views.index, name=app_name),
+    path("/", views.index, name=app_name),
+    path("login", views.user_login, name=app_name),
 ]
--- Python/mamba/template/templates/project_type/django/src/django_mamba/django_mamba_app/views.py
+++ Python/mamba/template/templates/project_type/django/src/django_mamba/django_mamba_app/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login
+
+#Utility
+
+def return_redirect(route):
+    return HttpResponseRedirect(reverse(route))
+
+def get_login_landing_page(user):
+    return ''
+
+#Actual views
 
 def index(request):
+    if request.user and not request.user.is_anonymous:
+        user = request.user
+        return render(request, 'landing.html', context={'user_first_name':user.first_name})
     return render(request, 'landing.html')
 
-def login(request):
+def user_login(request):
+    if request.POST:
+        username = request.POST['user']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
     return render(request, 'login.html')
